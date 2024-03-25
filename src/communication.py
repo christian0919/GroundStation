4c7bd42744864dbf7a2007ddcf77be71c1a443a7
Christian del Angel <christian.lopez190900@gmail.com>
2024-03-25 02:25:26 -0600

    🔄communication(Get_Cansat_Data)✏️

--- src/communication.py
+++ src/communication.py
@@ -4,14 +4,18 @@ import time
 
 class communication:
     def __init__(self):
-        self.serial_Port = ""
-        self.puerto_serial = serial.Serial('COM5', 115200, timeout=1)
+        self.serial_port = serial.Serial('COM5', 115200, timeout=1)
 
     def List_Serial_Ports():
         return serial.tools.list_ports.comports()
 
     def Send_AT_Command(self,command):
-        self.puerto_serial.write((command + '\r\n').encode())  # Agrega el retorno de carro y la nueva línea requeridos por los comandos AT
+        self.serial_port.write((command + '\r\n').encode())  # Agrega el retorno de carro y la nueva línea requeridos por los comandos AT
         time.sleep(0.1)  #Espera breve para que el módulo responda
-        respuesta = self.puerto_serial.readlines()
+        respuesta = self.serial_port.readlines()
         return respuesta
+    
+    def Get_Cansat_Data(self):
+        data = self.serial_port.readline().decode().strip()
+        if(data != "" or data !=''):
+            print("Datos recibidos:", data)

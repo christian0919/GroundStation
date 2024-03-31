83f8c0ce3b664482b81e9be98b157900f1891236
Christian del Angel <christian.lopez190900@gmail.com>
2024-03-31 01:01:16 -0600

    setting finished 🎉

--- src/configView.py
+++ src/configView.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QDialog, QVBoxLayout
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile, Qt
-
+from config import config
 class configView(QDialog):
     def __init__(self):
         super().__init__()
@@ -15,12 +15,29 @@ class configView(QDialog):
         
         layout = QVBoxLayout()
         layout.addWidget(self.ui)
+
         self.setLayout(layout)
         # Conectar señales y slots
         self.ui.ok_button.clicked.connect(self.ok)
         self.ui.cancel_button.clicked.connect(self.cancel)
-    
+        self.configuration = config()
+        self.ui.spinAddr.setValue(self.configuration.get_LoRa_ADDRESS())
+        self.ui.spinNetwork.setValue(self.configuration.get_LoRa_NETWORKID())
+        self.ui.spinSpread.setValue(self.configuration.get_LoRa_SPREADING_FACTOR())
+        self.ui.spinBandwidth.setValue(self.configuration.get_LoRa_BANDWIDTH())
+        self.ui.spinCoding.setValue(self.configuration.get_LoRa_CODING_RATE())
+        self.ui.spinPreamble.setValue(self.configuration.get_LoRa_PROGRAMMED_PREAMBLE())
+
+
+
     def ok(self): 
+        self.configuration.Set_Values(self.ui.spinAddr.value(),
+                                      self.ui.spinNetwork.value(),
+                                      self.ui.comboBand.currentText(),
+                                      self.ui.spinSpread.value(),
+                                      self.ui.spinBandwidth.value(),
+                                      self.ui.spinCoding.value(),
+                                      self.ui.spinPreamble.value())
         self.accept()
     def cancel(self):
         self.accept()

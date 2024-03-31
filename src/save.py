12087995adce464eecaf40126a4cd217e2380c8d
Christian del Angel <christian.lopez190900@gmail.com>
2024-03-30 21:51:51 -0600

    🗞Save data cerated ⏳

---
+++ src/save.py
@@ -0,0 +1,27 @@
+import sys
+import csv
+from datetime import datetime
+class save:
+    def __init__(self):
+        self.data = []
+        self.__path = ""
+#        fecha_hora_formateada = fecha_hora_actual.strftime("%Y-%m-%d_%H-%M-%S")
+
+    def saveData(self):
+        with open(self.__path , "a", newline="") as archivo_csv:
+            writer = csv.writer(archivo_csv)
+
+            # Si el archivo está vacío, escribir la cabecera
+            if archivo_csv.tell() == 0:
+                writer.writerow(["Presion", "Velocidad", "Altura"])
+
+            # Escribir los datos en el archivo CSV
+            writer.writerows(self.data)
+
+    def setPath(self):
+        path = datetime.now()
+        path = path.strftime("%Y-%m-%d_%H-%M-%S")
+        self.__path = "Missions/" + path + ".csv"  
+
+    def getPath(self):
+        return self.__path

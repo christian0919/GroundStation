cd48aa80a86f473e46d0b5066fed72269bb548af
Christian del Angel <christian.lopez190900@gmail.com>
2024-03-30 20:10:24 -0600

    🔄GUI(get serial ports connected)👌

--- src/Main.py
+++ src/Main.py
@@ -3,6 +3,8 @@ from PySide6.QtWidgets import QApplication
 import pyqtgraph as pg
 import numpy as np
 import time
+import serial.tools.list_ports
+
 uiclass, baseclass = pg.Qt.loadUiType("Main_View.ui")
 
 class MainWindow(uiclass, baseclass):
@@ -32,25 +34,27 @@ class MainWindow(uiclass, baseclass):
         self.Graph_Velocidad = self.Graph_Velocidad.plot(pen=lineGraphicsColor)
         self.Graph_Humedad = self.Graph_Humedad.plot(pen=lineGraphicsColor)
 
-
+ 
+        self.buttonPorts.clicked.connect(self.UpdatePortsList)
+ 
         self.timer = pg.QtCore.QTimer()
         self.timer.timeout.connect(self.RandomData)
         self.timer.start(100)
 
     def RandomData(self):
         rnAltitud = np.random.uniform(0, 500)
-        print("rnAltitud:", rnAltitud)
+        #print("rnAltitud:", rnAltitud)
         self.y_Altitud.append(rnAltitud)
         self.x.append(len(self.x))
         self.Graph_Altitud.setData(self.x, self.y_Altitud)
 
         rnTemperatureI = np.random.uniform(15, 30)
-        print("rnTemperatureI:", rnTemperatureI)
+        #print("rnTemperatureI:", rnTemperatureI)
         self.y_InternTemp.append(rnTemperatureI)
         self.Graph_TemperaturaInterna.setData(self.x , self.y_InternTemp)
 
         rnTemperatureE = np.random.uniform(15, 30)
-        print("rnTemperatureE:", rnTemperatureE)
+        #print("rnTemperatureE:", rnTemperatureE)
         self.y_ExTemp.append(rnTemperatureE)
         self.Graph_Temperatura_Externa.setData(self.x , self.y_ExTemp)
         
@@ -59,11 +63,20 @@ class MainWindow(uiclass, baseclass):
         self.Graph_Humedad.setData(self.x , self.y_hum)
         
         rnVelocidad = np.random.uniform(0, 100)
-        print("rnVelocidad:", rnVelocidad)
+        #print("rnVelocidad:", rnVelocidad)
         self.y_vel.append(rnVelocidad)
         self.Graph_Velocidad.setData(self.x , self.y_vel)
 
-        
+    def UpdatePortsList(self):
+        # Limpiar la lista de puertos antes de actualizarla
+        self.comboPorts.clear()
+        # Obtener la lista de puertos seriales
+        puertos = serial.tools.list_ports.comports()
+        print(puertos)
+
+        # Agregar cada puerto a la lista
+        for puerto in puertos:
+            self.comboPorts.addItem(puerto.device)
 
 app = QApplication(sys.argv)
 window = MainWindow()
